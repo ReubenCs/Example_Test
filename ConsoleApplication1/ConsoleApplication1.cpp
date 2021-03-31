33cd8e02639958410442ac55323ee3a599954722
ReubenCserepka <reuben.cserepka@gmail.com>
2021-03-30 22:39:17 -0400

    Nested loops!

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -6,6 +6,15 @@
 int main()
 {
     std::cout << "Hello World!\n";
+
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			std::cout << "*";
+		}
+		std::cout << std::endl;
+	}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
